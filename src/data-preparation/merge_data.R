8d9fd93917f0204748848e9b41c1f36106118ac9
Lisa Holling <l.holling@tilburguniversity.edu>
2023-03-18 16:28:30 +0100

    moved library loadings from merge to download

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,8 +1,6 @@
 #load("./gen/data-preparation/src/data-preparation/listing_type.csv")
 #load("./gen/data-preparation/src/data-preparation/listings_sorted.csv")
 
-library(magrittr)
-library(dplyr)
 ## Join the listings tibble with listings_sorted to add the listing_type column
 
 listings_joined <- inner_join(listings_sorted, listing_type, by = c("id" = "listing_id"))
