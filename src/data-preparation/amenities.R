3d50dfcc0768841a4c3bd591fa1682e859a82495
Lisa Holling <l.holling@tilburguniversity.edu>
2023-03-19 15:34:58 +0100

    I renamed all the files. Now, all lists and dfss for the first 100 amenities, have 100 behind them (e.g. wide_df100). And all amenities are stored in wide_df; so that we can also regress on all amenities

--- src/data-preparation/amenities.R
+++ src/data-preparation/amenities.R
@@ -17,6 +17,7 @@ amenities_df$amenities <- gsub(" ", "_", amenities_df$amenities)
 amenities_df$amenities <- gsub("-", "_", amenities_df$amenities)
 amenities_df$amenities <- gsub("\\\\", "", amenities_df$amenities)
 amenities_df$amenities <- gsub("/", "", amenities_df$amenities)
+amenities_df$amenities <- gsub(":", "_", amenities_df$amenities)
 
 # Create amenity_counts_(short/long)
 ## Count the frequency of each amenity
@@ -50,17 +51,38 @@ amenity_counts_long <- amenity_counts_long %>%
 ## Join two df with counts
 counts_long_short <- inner_join(amenity_counts_long, amenity_counts_short, by = c("amenities"), suffix = c("_long","_short"))
 
+
+
+# Dataset: all amenities
+
+## Create regression subset with all amenities
+amenities <- counts_long_short$amenities
+
+all_amenities <- amenities_df %>%
+  filter(amenities %in% amenities)
+
+## Numeric  
+wide_df <- pivot_wider(all_amenities, 
+                          id_cols = c("id", "listing_type"), 
+                          names_from = "amenities", 
+                          values_from = "amenities", 
+                          values_fn = function(x) as.integer(length(x) > 0), 
+                          values_fill = 0, 
+                          names_prefix = "has_")
+
+
+# Dataset: only first 100 amenities
 ## Filtering for the first 100 amenities
-counts_long_short <- counts_long_short %>% slice(1:100)
-common_amenities <- counts_long_short$amenities
+counts_long_short100 <- counts_long_short %>% slice(1:100)
+common_amenities100 <- counts_long_short100$amenities
 
 # Create regression subset with only largest 100 amenities
 ## Filter amenities of amenities_df such that only the first 100 of counts_long_short maintain
-filtered_amenities_df <- amenities_df %>%
-  filter(amenities %in% common_amenities)
+filtered_amenities_df100 <- amenities_df %>%
+  filter(amenities %in% common_amenities100)
 
 ## From long to wide dataframe
-wide_df <- pivot_wider(filtered_amenities_df, 
+wide_df100 <- pivot_wider(filtered_amenities_df100, 
                        id_cols = c("id", "listing_type"), 
                        names_from = "amenities", 
                        values_from = "amenities", 
@@ -69,13 +91,14 @@ wide_df <- pivot_wider(filtered_amenities_df,
                        names_prefix = "has_")
 
 ## Numeric instead of yes and no 
-wide_df <- pivot_wider(filtered_amenities_df, 
+wide_df100 <- pivot_wider(filtered_amenities_df100, 
                        id_cols = c("id", "listing_type"), 
                        names_from = "amenities", 
                        values_from = "amenities", 
                        values_fn = function(x) as.integer(length(x) > 0), 
                        values_fill = 0, 
                        names_prefix = "has_")
+
 
 
 # Create counts_100
@@ -92,4 +115,15 @@ counts_100 <- counts_100 %>%
 # Output of this R-file for regression
 ## wide_df
 ## counts_100
+
+
+# Ignore below/don't remove pls :)
+##short_count <- wide_df %>%  filter(listing_type == "short")  %>% count(has_Essentials)
+##short_count
+##short_count0 <- short_count %>% filter(has_Essentials == 0) 
+##short_count0$n
+##short_count1 <- short_count %>% filter(has_Essentials == 1) 
+##short_count1$n
+##short_proportion <- short_count0$n/sum(short_count1$n, short_count0$n)
+##wide_df %>%  filter(listing_type == "long") %>% count(has_Essentials)
 
