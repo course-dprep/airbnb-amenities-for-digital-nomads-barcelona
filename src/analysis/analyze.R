3b82d9f8d152b122b71e012cb53746ddacc213be
Lisa Holling <l.holling@tilburguniversity.edu>
2023-03-19 16:32:19 +0100

    I completed the regression for the 1st 100 amenities; working on all amenities

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -11,42 +11,119 @@
 #save(m1,m2,file="./gen/analysis/output/model_results.RData")
 
 
-# H0: count_long = count_short
-# H1: count_long > count_short 
+# FOR FIRST 100 AMENITIES
+# Create list of dependent variables, i.e. all 100 amenities
+dep_vars <- names(wide_df100[3:102])
 
-# long:  ; short: 3615
-sum(length(wide_df$listing_type["short"]))
-length(listings_long) + length(listings_short)
-
-short_count <- wide_df %>%  filter(listing_type == "short")  %>% count(has_Essentials)
-short_count
-short_count0 <- short_count %>% filter(has_Essentials == 0) 
-short_count0$n
-short_count1 <- short_count %>% filter(has_Essentials == 1) 
-short_count1$n
-short_proportion <- short_count0$n/sum(short_count1$n, short_count0$n)
-
-wide_df %>%  filter(listing_type == "long") %>% count(has_Essentials)
-
-# regression for has_Essentials
-summary(lm(has_Essentials ~ listing_type, wide_df))
-summary(lm(has_Crib ~ listing_type, wide_df))
-
-
-dep_vars <- names(wide_df[3:100])
-names(wide_df)
-wide_df
-dep_vars
-
-# Create an empty list to store the output
-output_list <- list()
-
-# Loop over the dependent variables and fit a linear regression model for each
+# Loop over each dependent variable and run regression
 for (dep_var in dep_vars) {
-  formula <- paste(dep_var, "~ listing_type")
-  model <- lm(formula, data = wide_df)
-  output_list[[dep_var]] <- summary(model)
+  str <- paste(dep_var, "~ listing_type")  
+  model <- lm(str, wide_df100)
+  print(summary(model))
 }
 
-# Print the output list
-print(output_list)
+###### TO ILLUSTRATE
+t <- summary(lm(has_Crib ~ listing_type, wide_df100))
+t
+t$coefficients[2,1] # listing_typeshort 0.088444
+# this means, that if listing_type = short, estimate of having that amenity increases with 0.08
+# therefore, if we want amenities that are significantly more in long listings, this coefficient must be negative
+######
+
+# Create a list with only significant amenities
+## empty list
+significant_amenities_negative100 <- c()
+significant_amenities_positive100 <- c()
+
+## loop as before
+for (dep_var in dep_vars) {
+  str <- paste(dep_var, "~ listing_type")  
+  model <- lm(str, wide_df100)
+  summary <- summary(model)
+
+  ## check for which amenities p < 0.05 (significant)
+  if (summary$coefficients[2, 4] < 0.05) {
+    ### if effect is negative
+    if (summary$coefficients[2, 1] < 0) {
+      significant_amenities_negative100 <- c(significant_amenities_negative100, dep_var)
+      ### if effect is positive
+    } else if (summary$coefficients[2, 1] > 0) {
+      significant_amenities_positive100 <- c(significant_amenities_positive100, dep_var)
+    }
+  }
+}
+
+# Total number of significant amenities
+length(significant_amenities_negative100) + length(significant_amenities_positive100)
+
+# Amenities significantly more available in short listings
+print(significant_amenities_negative100)
+length(significant_amenities_negative100)
+
+# Amenities significantly more available in long listings
+print(significant_amenities_positive100)
+length(significant_amenities_positive100)
+
+# examples positives: more with short listings
+summary(lm(has_Dryer ~ listing_type, wide_df100))
+summary(lm(has_Bidet ~ listing_type, wide_df100))
+summary(lm(has_Oven ~ listing_type, wide_df100))
+summary(lm(has_Safe ~ listing_type, wide_df100))
+summary(lm(has_Lockbox ~ listing_type, wide_df100))
+
+# examples negatives: more with long listings
+summary(lm(has_Essentials ~ listing_type, wide_df100))
+summary(lm(has_Kitchen ~ listing_type, wide_df100))
+summary(lm(has_Wifi ~ listing_type, wide_df100))
+summary(lm(has_Baby_bath ~ listing_type, wide_df100))
+
+# Save as lists
+write_csv(significant_amenities_positive100, "significant_amenities_long_100")
+write_csv(significant_amenities_negative100, "significant_amenities_short_100")
+
+# FOR ALL AMENITIES        
+
+# Create list of dependent variables, i.e. all 1859 amenities
+dep_vars <- names(wide_df[3:1859])
+
+# Create a list with only significant amenities
+## empty list
+significant_amenities_negative <- c()
+significant_amenities_positive <- c()
+
+## loop as before
+for (dep_var in dep_vars) {
+  if (dep_var %in% wide_df) {  # add this check
+    str <- paste(dep_var, "~ listing_type")
+    model_data <- wide_df[, c("listing_type", dep_var)]
+    model_data <- model_data[complete.cases(model_data), ]
+    model <- lm(as.formula(str), data = model_data)
+    summary <- summary(model)
+    
+    ## check for which amenities p < 0.05 (significant)
+    if (summary$coefficients[2, 4] < 0.05) {
+      ### if effect is negative
+      if (summary$coefficients[2, 1] < 0) {
+        significant_amenities_negative <- c(significant_amenities_negative, dep_var)
+        ### if effect is positive
+      } else if (summary$coefficients[2, 1] > 0) {
+        significant_amenities_positive <- c(significant_amenities_positive, dep_var)
+      }
+    }
+  }
+} 
+
+# Total number of significant amenities
+length(significant_amenities_negative) + length(significant_amenities_positive)
+
+# Amenities significantly more available in short listings
+print(significant_amenities_negative)
+length(significant_amenities_negative)
+
+# Amenities significantly more available in long listings
+print(significant_amenities_positive)
+length(significant_amenities_positive)
+
+# save as lists
+write_csv(significant_amenities_positive, "significant_amenities_long")
+write_csv(significant_amenities_negative, "significant_amenities_short"
