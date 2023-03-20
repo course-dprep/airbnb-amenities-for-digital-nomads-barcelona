56d344bf8c5985231bd6944dfec0f2c3de007923
Lisa Holling <l.holling@tilburguniversity.edu>
2023-03-20 13:00:29 +0100

    finalized regression for first 100 amenities

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -56,11 +56,11 @@ for (dep_var in dep_vars) {
 # Total number of significant amenities
 length(significant_amenities_negative100) + length(significant_amenities_positive100)
 
-# Amenities significantly more available in short listings
+# Amenities significantly more available in long listings
 print(significant_amenities_negative100)
 length(significant_amenities_negative100)
 
-# Amenities significantly more available in long listings
+# Amenities significantly more available in short listings
 print(significant_amenities_positive100)
 length(significant_amenities_positive100)
 
@@ -77,6 +77,9 @@ summary(lm(has_Kitchen ~ listing_type, wide_df100))
 summary(lm(has_Wifi ~ listing_type, wide_df100))
 summary(lm(has_Baby_bath ~ listing_type, wide_df100))
 
+significant_amenities_positive100 <- data.frame(significant_amenities_positive100)
+significant_amenities_negative100 <- data.frame(significant_amenities_negative100)
+
 # Save as lists
 write_csv(significant_amenities_positive100, "significant_amenities_long_100")
 write_csv(significant_amenities_negative100, "significant_amenities_short_100")
@@ -84,7 +87,7 @@ write_csv(significant_amenities_negative100, "significant_amenities_short_100")
 # FOR ALL AMENITIES        
 
 # Create list of dependent variables, i.e. all 1859 amenities
-dep_vars <- names(wide_df[3:1859])
+dep_vars <- names(wide_df[3:10])
 
 # Create a list with only significant amenities
 ## empty list
@@ -93,11 +96,9 @@ significant_amenities_positive <- c()
 
 ## loop as before
 for (dep_var in dep_vars) {
-  if (dep_var %in% wide_df) {  # add this check
-    str <- paste(dep_var, "~ listing_type")
-    model_data <- wide_df[, c("listing_type", dep_var)]
-    model_data <- model_data[complete.cases(model_data), ]
-    model <- lm(as.formula(str), data = model_data)
+  if (dep_var %in% wide_df) { 
+    str <- paste(dep_var, "~ listing_type")  
+    model <- lm(str, wide_df100)
     summary <- summary(model)
     
     ## check for which amenities p < 0.05 (significant)
@@ -113,17 +114,42 @@ for (dep_var in dep_vars) {
   }
 } 
 
+## loop as before
+# for (dep_var in dep_vars) {
+ # if (dep_var %in% wide_df) {  # add this check
+#    str <- paste(dep_var, "~ listing_type")
+#    model_data <- wide_df[, c("listing_type", dep_var)]
+#    model_data <- model_data[complete.cases(model_data), ]
+#    model <- lm(as.formula(str), data = model_data)
+#    summary <- summary(model)
+#    
+    ## check for which amenities p < 0.05 (significant)
+#    if (summary$coefficients[2, 4] < 0.05) {
+#      ### if effect is negative
+#      if (summary$coefficients[2, 1] < 0) {
+#        significant_amenities_negative <- c(significant_amenities_negative, dep_var)
+#        ### if effect is positive
+#      } else if (summary$coefficients[2, 1] > 0) {
+#        significant_amenities_positive <- c(significant_amenities_positive, dep_var)
+#     }
+#   }
+#  }
+# } # Terminates R...
+
 # Total number of significant amenities
-length(significant_amenities_negative) + length(significant_amenities_positive)
+# length(significant_amenities_negative) + length(significant_amenities_positive)
 
 # Amenities significantly more available in short listings
-print(significant_amenities_negative)
-length(significant_amenities_negative)
+# print(significant_amenities_negative)
+# length(significant_amenities_negative)
 
 # Amenities significantly more available in long listings
-print(significant_amenities_positive)
-length(significant_amenities_positive)
+# print(significant_amenities_positive)
+# length(significant_amenities_positive)
 
-# save as lists
-write_csv(significant_amenities_positive, "significant_amenities_long")
-write_csv(significant_amenities_negative, "significant_amenities_short"
+# Save as lists
+#write_csv(significant_amenities_positive, "significant_amenities_long")
+#write_csv(significant_amenities_negative, "significant_amenities_short")
+
+#write_csv(significant_amenities_positive, "significant_amenities_long")
+#write_csv(significant_amenities_negative, "significant_amenities_short"
