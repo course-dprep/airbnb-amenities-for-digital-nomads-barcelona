2c4e42c17deb33e5ec5dc65a79ed3780d3c6e45e
Roos Van Sambeek <roosvansambeek@we0369.uvt.nl>
2023-03-22 13:20:24 +0100

    changes to makefile in 4-1 map

--- src/data-preparation/amenities_FA.R
+++ src/data-preparation/amenities_FA.R
@@ -23,7 +23,7 @@ library(dplyr)
 df_short <- select(df_short, -which(colSums(is.na(df_short)) == nrow(df_short)))
 
 # run factor analysis
-fa_short <- psych::fa(df_short, nfactors = 3)
+fa_short <- psych::fa(df_short, nfactors = 1000)
 
 # run factor analysis
 fa_short <- psych::fa(df_short, nfactors = 3)
@@ -40,7 +40,19 @@ fa_long <- psych::fa(df_long, nfactors = 3)
 # # subset for long-term listings
 # df_long <- subset(df, listing_type == "long")
 # 
-# 
+counts_long_short <- counts_long_short %>% 
+  mutate(prop_long = count_long / (count_long + count_short))
+
+counts_long_short <- counts_long_short %>% 
+  mutate(prop_short = count_short / (count_long + count_short))
+
+#from long to wide pivot
+head(counts_long_short)
+wide_df100 <- pivot_wider(counts_long_short, 
+                          id_cols = c("prop_long", "prop_short"), 
+                          names_from = "amenities", 
+                          values_from = "amenities")
+
 # # probabilities long
 # df_factor_analysis <- df_factor_analysis %>%
 #   mutate(prop_long = count_long / (count_long + count_short))
@@ -51,3 +63,63 @@ fa_long <- psych::fa(df_long, nfactors = 3)
 # 
 # #write csv
 # write_csv(df_factor_analysis, "../../gen/analysis/input/df_factor_analysis.csv")
+
+head(wide_df)
+
+wide_df_counts <- wide_df %>% 
+  summarize(count = n())
+
+
+#dataset for factor analysis
+amenity_counts_short <- amenities_df %>% 
+  filter(listing_type == "short")
+
+## Count the frequency of each amenity for short stays
+amenity_counts_short <- amenity_counts_short %>% 
+  group_by(amenities) %>% 
+  summarize(count = n()) %>% 
+  arrange(desc(count))
+
+## Filter for long stays
+amenity_counts_long <- amenities_df %>% 
+  filter(listing_type == "long")
+
+## Count the frequency of each amenity for long stays
+amenity_counts_long <- amenity_counts_long %>% 
+  group_by(amenities) %>% 
+  summarize(count = n()) %>% 
+  arrange(desc(count))
+
+## Join two df with counts
+counts_long_short <- inner_join(amenity_counts_long, amenity_counts_short, by = c("amenities"), suffix = c("_long","_short"))
+
+# probabilities
+counts_long_short <- counts_long_short %>%
+  mutate(prop_long = count_long / (count_long + count_short))
+
+counts_long_short <- counts_long_short %>%
+  mutate(prop_short = count_short / (count_long + count_short))
+
+df_factors <- counts_long_short[, c("amenities", "prop_long", "prop_short")]
+
+# dataframe for long
+df_factors_long <- counts_long_short[, c("amenities", "prop_long")]
+
+# dataframe for short 
+df_factors_short <- counts_long_short[, c("amenities", "prop_short")]
+
+library(psych)
+
+# Assuming your dataset is called "amenities_data"
+# Convert the "amenities" column to a rownames
+rownames(df_factors_short) <- df_factors_short$amenities
+df_factors_short$amenities <- NULL
+
+# Perform factor analysis
+fa <- fa(df_factors_short, nfactors = 2, rotate = "varimax")
+
+# View the results
+print(fa, sort = TRUE)
+
+
+                          
